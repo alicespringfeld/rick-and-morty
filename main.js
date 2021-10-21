e8d1a3d50b1c405c7e8b566393abb3ce623bb24d
alicespringfeld <alice.springfeld@yahoo.com>
2021-10-21 10:55:01 +0200

    :art: "Removed comments and renamed element"
    I added some changes after the review.

--- main.js
+++ main.js
@@ -12,25 +12,6 @@ async function createApp() {
   const body = await response.json();
   const characters = body.results;
 
-  /*const characters = [
-    {
-      name: "Rick",
-      status: "Death",
-      race: "Alien",
-      location: "Earth",
-      image:
-        "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.8OjjdUN8xCiccTkUtcwIHgHaJV%26pid%3DApi&f=1",
-    },
-    {
-      name: "Morty",
-      status: "Alive",
-      race: "Alien",
-      location: "Mars",
-      image:
-        "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.mPRQa5i7FgrOmGOT2mvFNwHaHA%26pid%3DApi&f=1",
-    },
-  ];*/
-
   const characterElement = characters.map((character) =>
     createCharacterComponent(character)
   );
@@ -40,24 +21,6 @@ async function createApp() {
     { className: "main" },
     characterElement
   );
-
-  /* createCharacterComponent({
-      name: "Rick",
-      status: "Death",
-      race: "Alien",
-      location: "Earth",
-      image:
-        "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.8OjjdUN8xCiccTkUtcwIHgHaJV%26pid%3DApi&f=1",
-    }),
-    createCharacterComponent({
-      name: "Morty",
-      status: "Alive",
-      race: "Alien",
-      location: "Mars",
-      image:
-        "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.mPRQa5i7FgrOmGOT2mvFNwHaHA%26pid%3DApi&f=1",
-    }),
-  ]);*/
 
   const footerElement = createElement("footer", { className: "footer" }, [
     createElement("p", {
