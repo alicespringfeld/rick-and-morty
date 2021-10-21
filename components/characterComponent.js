c07b8508da96441e25a469120a36443a2303f1bf
alicespringfeld <91462529+alicespringfeld@users.noreply.github.com>
2021-10-21 10:40:20 +0200

    Update components/characterComponent.js
    Co-authored-by: Lara <82370324+lajanzen@users.noreply.github.com>

--- components/characterComponent.js
+++ components/characterComponent.js
@@ -48,7 +48,6 @@ export default function createCharacterComponent({
   fetch(firstEpisode)
     .then((response) => response.json())
     .then((body) => {
-      /*console.log(body.name);*/
       firstEpisodeElement.textContent = body.name;
     });
 
