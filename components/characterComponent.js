0e7175c7996650156a86a17c9108ce2f93bbf8b2
alicespringfeld <alice.springfeld@yahoo.com>
2021-10-21 09:26:22 +0200

    Fetch characters and location from API

--- components/characterComponent.js
+++ components/characterComponent.js
@@ -5,9 +5,18 @@ export default function createCharacterComponent({
   name,
   image,
   status,
-  race,
+  species,
   location,
+  episode,
 }) {
+  const lastLocation = location[0];
+  const lastKnownLocationElement = createElement("p", {
+    textContent: location.name,
+  });
+
+  const firstEpisode = episode[0];
+  const firstEpisodeElement = createElement("p", { textContent: firstEpisode });
+
   const characterElement = createElement(
     "article",
     {
@@ -19,25 +28,30 @@ export default function createCharacterComponent({
         alt: "",
         className: styles.image,
       }),
-      createElement(
-        "h2",
-        {
-          textContent: name,
-          className: styles.headline,
-        },
-        [
-          createElement("p", { textContent: `${status}- ${race}` }),
-          createElement("h4", {
-            textContent: "Last known location: " + location,
-            className: styles.subhead,
-          }),
-        ]
-      ),
-      /*createElement("h4", {
-        textContent: location,
-        className: styles.subhead,
-      }),*/
+      createElement("h2", {
+        textContent: name,
+        className: styles.headline,
+      }),
+      createElement("p", { textContent: `${status} - ${species}` }),
+      createElement("h4", { textContent: "Last known location: " }),
+      lastKnownLocationElement,
+      createElement("h4", { textContent: "First seen in: " }),
+      firstEpisodeElement,
     ]
   );
+  fetch(lastLocation)
+    .then((response) => response.json())
+    .then((body) => {
+      console.log(body.name);
+      lastKnownLocationElement.textContent = body.name;
+    });
+
+  fetch(firstEpisode)
+    .then((response) => response.json())
+    .then((body) => {
+      console.log(body.name);
+      firstEpisodeElement.textContent = body.name;
+    });
+
   return characterElement;
 }
--- main.js
+++ main.js
@@ -3,13 +3,45 @@ import { createHeaderComponent } from "./lb/titleComponent";
 import "./style.css";
 import createCharacterComponent from "./components/characterComponent";
 
-function createApp() {
+async function createApp() {
   const appElement = document.querySelector("#app");
 
   const headerElement = createHeaderComponent();
 
-  const mainElement = createElement("main", { className: "main" }, [
-    createCharacterComponent({
+  const response = await fetch("https://rickandmortyapi.com/api/character");
+  const body = await response.json();
+  const characters = body.results;
+
+  /*const characters = [
+    {
+      name: "Rick",
+      status: "Death",
+      race: "Alien",
+      location: "Earth",
+      image:
+        "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.8OjjdUN8xCiccTkUtcwIHgHaJV%26pid%3DApi&f=1",
+    },
+    {
+      name: "Morty",
+      status: "Alive",
+      race: "Alien",
+      location: "Mars",
+      image:
+        "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.mPRQa5i7FgrOmGOT2mvFNwHaHA%26pid%3DApi&f=1",
+    },
+  ];*/
+
+  const characterElement = characters.map((character) =>
+    createCharacterComponent(character)
+  );
+
+  const mainElement = createElement(
+    "main",
+    { className: "main" },
+    characterElement
+  );
+
+  /* createCharacterComponent({
       name: "Rick",
       status: "Death",
       race: "Alien",
@@ -25,7 +57,7 @@ function createApp() {
       image:
         "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.mPRQa5i7FgrOmGOT2mvFNwHaHA%26pid%3DApi&f=1",
     }),
-  ]);
+  ]);*/
 
   const footerElement = createElement("footer", { className: "footer" }, [
     createElement("p", {
